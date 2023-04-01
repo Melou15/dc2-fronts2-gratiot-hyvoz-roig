a17dfc31a083b0a702f87b6cbbe996d64a19f290
Melou15 <114467800+Melou15@users.noreply.github.com>
2023-04-02 00:37:26 +0200

    Add files via upload
    Footer et Header modifier en css.
    
    Le header à tout ses composants, au fur et a mesure il nous faudra rajouter les différentes liens direct sur nos différentes pages. 
    
    Le footer possède toutes les informations nécéssaires. 
    
    Construction de la page d'accueil, un logo à été ajouté ainsi qu'une bannière, une première section à été réalisé (qui sommes nous)

--- footer.php
+++ footer.php
@@ -1,45 +1,97 @@
 <style>
-    #footer{
-    background-color: #F2F2F2;
-    margin: auto;
-    max-width: 1440px;
-    width: auto;
+  #footer{
+    height: auto;
+    padding-left: 20px;
+    padding-right: 20px;
+    background-color: #F3C9D3;
+    }
+
+footer{
     display: flex;
-    justify-content: space-between;
-    padding: 30px 3.8% 0 3.8%;
+    flex-wrap: wrap;
+    flex-direction: row;
+    align-items: center;
+    margin-top: 40px;
 }
-footer section{
-    padding: 1em 1em;
+
+footer section{ 
+    padding: 10px;
     width: 30%;
+    text-decoration: none;
 }
+
 footer section h2{
     margin-bottom: 10px;
-    font-size: 1.2em;
+    font-size: 15px;
+    text-decoration: none;
 }
 footer section p{
     margin-top: 20px;
     cursor: pointer;
-    width: fit-content;
+    width: auto;
+    text-decoration: none;
 }
-footer section p:hover{
-    border-bottom: 1px solid black;
+
+.fa-solid, .fa-brands{
+    color: #F4315B;
+
+} 
+
+.paiement p{
+    display: inline-flex;
+    padding-right: 15px;
 }
+
+.paiement p:hover{
+    text-shadow: 1px 1px 2px, 0 0 1em, 0 0 0.2em #FC0690;
+}
+
+.mentions_legale{
+    text-align: center;
+    color: grey;
+    font-size: 10px;
+}
+
+
 </style>
 <footer id="footer">
-            <section>
-                <h2>Nous contacter, nous rencontrer</h2>
-                <p><i class="fa-solid fa-phone"></i>0604084412</p>
-                <p><i class="fa-solid fa-house"></i>155 rue aung 34000 Montpellier</p>
-                <p></p>
-            </section>
-            <section>
-                <h2>Nos moyen de paiement</h2>
-                <p>bhggh</p>
-                <p>logo edjzeghdkze</p>
-            </section>
-            <section>
-                <h2>Nos réseaux sociaux</h2>
-                <p><i class="fa-brands fa-instagram"></i></p>
-                <p><i class="fa-brands fa-linkedin"></i></p>
-            </section>
-        </footer>
+    <section>
+        <h2>Nous contacter</h2>
+        <p><i class="fa-solid fa-phone"></i> 06 04 08 44 12</p>
+        <p><i class="fa-solid fa-house"></i> 155 Rue Aung San Suuki <br> --- 34000 Montpellier</p>
+        <p></p>
+    </section>
+    <section>
+        <h2>Nos moyens de paiement</h2>
+        <div class="paiement">
+            <p><i class="fa-brands fa-paypal fa-xl"></i></p>
+            <p><i class="fa-brands fa-google-pay fa-2x"></i></p>
+            <p><i class="fa-brands fa-apple-pay fa-2x"></i></p>
+            <p><i class="fa-brands fa-cc-mastercard fa-2x"></i></p>
+        </div>
+    </section>
+    <section>
+        <h2>Nos réseaux sociaux</h2>
+        <p><i class="fa-brands fa-instagram fa-xl"></i> @duttifruits</p>
+        <p><i class="fa-brands fa-linkedin fa-xl"></i> Dutti Fruits</p>
+    </section>
+    <div class="mentions_legale">
+
+<p>Les données personnelles importées sur ce site sont prélevées dans le respect de la loi relative à l'informatique, aux fichiers et aux libertés. Les informations requises ne seront en aucun cas transmises à des tiers. Vous êtes susceptible d'un droit d'accès, de modification, de rectification et de suppression des données qui vous concernent. 
+    <br>Vous pouvez exercer ce droit en voyant un e-mail à <a href="#">dutty_fruit@wanadoo.com.</a></p>
+
+<p>Nous vous remercions de votre visite et de votre confiance.</p>
+
+<p>- GRATIOT-FLEURANT Charlotte - HYVOZ Audrey - ROIG Mélissa -</p>
+    </div>
+</footer>
+
+
+
+
+
+
+
+
+
+
--- header.php
+++ header.php
@@ -1,28 +1,59 @@
 <style>
+
+	#entete 
+	{
+		display: flex;
+	    flex-direction: row;
+	    align-items: flex-end;
+	    justify-content: space-between;
+	    padding-left: 15px;
+	    padding-right: 15px;
+	    margin-top: 20px;
+	}
+
+.logo{
+		width: 50px;
+		height: auto;
+	}
 	nav ul
-{
-    list-style-type: none;
-    display: flex;
-    justify-content: center;
+	{
+		display: flex;
+		justify-content: flex-end;
 
-}
-nav li
-{
-    margin-right: 15px;
-}
+	}
 
+	nav a
+	{
+		font-size:15px;
+		align-content: center;
+    	text-decoration: none;
+	}
+
+	a:hover {
+		color : #F4315B;
+	}
+
+		nav li
+	{
+		margin-right: 15px;
+		list-style-type: none;
+
+	}
 </style>
-<header>
-		<div>
-			<nav>
-				<ul>
-					<li><a href="">Produits</a></li>
-					<li><a href="#">Panier</a></li>
-					<li><a href="#">Clients</a></li>
-					<li><a href="#">Contacts</a></li>
-					<li><a href="ajoutprod.php">Ajouter un produit</a></li>
-					<i class="fa-solid fa-magnifying-glass"></i>
-				</ul>
-			</nav>
-		</div>
-	</header>
+<header id="entete">
+	<div>
+		<img class="logo" src="images/logo-dutty-fruit.png" alt="logo-dutty-fruit">
+	</div>
+	<div>	
+		<nav>
+			<ul>
+				<li><a href="#">PRODUITS</a></li>
+				<li><a href="#">PANIER</a></li>
+				<li><a href="#">CLIENTS</a></li>
+				<li><a href="#">CONTACT</a></li>
+				<li><a href="ajoutprod.php">Ajouter un produit</a></li>
+				<a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+			</ul>
+		</nav>
+	</div>
+</header>
