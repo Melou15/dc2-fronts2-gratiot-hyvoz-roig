6911032c4fb74ad69c18f5aa9e64c2355cd943af
Melou15 <114467800+Melou15@users.noreply.github.com>
2023-04-02 01:20:13 +0200

    Add files via upload
    Ajout de la partie avec les différentes cartes fruits, clicables mais ne renvoie pas encore a la page

--- accueil.php
+++ accueil.php
@@ -5,49 +5,113 @@ include 'db.class.php';
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initiale-scale=1">
-	<script src="https://kit.fontawesome.com/3f07be2185.js" crossorigin="anonymous"></script>
+	<meta name="viewport" content="width=device-width">
+	<script src="https://kit.fontawesome.com/2c70a62eec.js" crossorigin="anonymous"></script>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;500&display=swap" rel="stylesheet">
 	<title>HYVOZ-GRATIOT-ROIG</title>
 </head>
 <body>
-	<script src="./siteecommerce.js"></script>
 	<?php include 'header.php';?>
 	<?php include 'accueilcss.php' ;?>
-	<h2> Présentation </h2>
-	<section class="position_fruits">
-		<div class="fichefruit">
-			<img src="images/fraise.png">
-			<p>Fraise</p>
-			<p>1€</p>
 
-		</div>
-		<div class="fichefruit">
-			<img src="images/framboise.png">
-			<p>Framboise</p>
-			<p>2€</p>
-		</div>
-		<div class="fichefruit">
-			<img src="images/mangue.png">
-			<p>Mangue</p>
-			<p>3€</p>
-		</div>
-		<div class="fichefruit">
-			<img src="images/poire.png">
-			<p>Poire</p>
-			<p>4€</p>
-		</div>
-		<div class="fichefruit">
-			<img src="images/raisin.png">
-			<p>Raisin</p>
-			<p>5€</p>
-		</div>
-		<div class="fichefruit">
-			<img src="images/banane.png">
-			<p>Banane</p>
-			<p>6€</p>
-		</div>
-	</section>
+	<img class="banière" src="images/banière-dutty-fruit.png" alt="banière-dutty-fruit">
 
-	<?php include 'footer.php';?>
-</body>
-</html>
+	<h2> Qui sommes-nous ?</h2>
+
+	<div class="presentation">
+
+		<div><img class="img_pres" src="images/presentation.png" alt="personne travaillant dans les champs">
+		</div>
+
+		<div class="txt_pres">
+			<p>Notre entreprise, Dutti-Fruits, est une société à responsabilité limitée (SARL) spécialisée dans la vente de fruits. Nous proposons une large gamme de fruits frais, de saison et de qualité supérieure, soigneusement sélectionnés auprès de producteurs locaux et régionaux.
+
+				<br>Notre objectif est de fournir à nos clients des fruits frais, sains et savoureux, tout en soutenant les producteurs locaux et en encourageant une agriculture durable. Nous sommes passionnés par la qualité de nos produits et nous nous engageons à offrir un service client exceptionnel à chaque étape de l'achat.</p>
+
+				<p>Chez Dutti-Fruits, nous sommes convaincus que manger des fruits frais est essentiel pour une vie saine et équilibrée. C'est pourquoi nous sommes fiers de proposer une large sélection de fruits, du plus commun au plus exotique, afin de répondre aux goûts et aux besoins de chacun.
+
+					<br>Nous sommes ravis de partager notre passion pour les fruits avec vous et nous nous engageons à fournir des produits frais, sains et savoureux pour votre plus grand plaisir.</p>
+				</div>
+			</div>
+
+			<h2>Nos fruits</h2>	
+			<section class="position_fruits">
+
+
+				<div class="fichefruit">
+					<a href="#">
+						<div class="fraise"></div>	
+						<div>
+							<img src="images/fraise.png">
+							<p>FRAISE</p>
+							<p> 1€ </p>
+						</div>
+					</a>
+				</div>
+
+				<div class="fichefruit">
+					<a href="#">
+						<div class="framboise"></div>	
+						<div>
+							<img src="images/framboise.png">
+							<p>FRAMBOISE</p>
+							<p> 2€ </p>
+						</div>
+					</a>
+				</div>
+
+				<div class="fichefruit">
+					<a href="#">
+						<div class="mangue"></div>	
+						<div>
+							<img src="images/mangue.png">
+							<p>MANGUE</p>
+							<p> 3€ </p>
+						</div>
+					</a>
+				</div>
+
+				<div class="fichefruit">
+					<a href="#">
+						<div class="poire"></div>	
+						<div>
+							<img src="images/poire.png">
+							<p>POIRE</p>
+							<p> 4€ </p>
+						</div>
+					</a>
+				</div>
+
+				<div class="fichefruit">
+					<a href="#">
+						<div class="raisin"></div>	
+						<div>
+							<img src="images/raisin.png">
+							<p>RAISIN</p>
+							<p> 5€ </p>
+						</div>
+					</a>
+				</div>
+
+				<div class="fichefruit">
+					<a href="#">
+						<div class="banane"></div>	
+						<div>
+							<img src="images/banane.png">
+							<p>BANANE</p>
+							<p> 6€ </p>
+						</div>
+					</a>
+				</div>
+			</section>
+
+			<p class="citation">"Un fruit mûr et savoureux est la preuve que la nature peut nous offrir à la fois le plaisir et la santé." - S. Khosla</p>
+
+			<h2>Ils nous ont fait confiance</h2>
+
+
+			<?php include 'footer.php';?>
+		</body>
+		</html>
